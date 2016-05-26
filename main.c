cfc3222fb2f1ec6028f768cc3904d91717962abc
qsctr <brettonchen@gmail.com>
2016-05-26 14:02:11 +0800

    Correct drive motor directions and slow down rotation

--- main.c
+++ main.c
@@ -37,12 +37,12 @@
 
 void drive() {
 
-    int x = vexRT[Ch1], y = vexRT[Ch2], r = vexRT[Ch4];
+    int x = vexRT[Ch4], y = vexRT[Ch3], r = vexRT[Ch1] / 2;
 
     motor[FL] = - y - x - r;
-    motor[FR] = + y - x - r;
-    motor[BR] = + y + x - r;
-    motor[BL] = - y + x - r;
+    motor[FR] = - y + x + r;
+    motor[BR] = - y - x + r;
+    motor[BL] = + y - x + r;
 
 }
 
