ccc3a672cd03b7f08b4567c9e04ea2dfe8b7774a
qsctr <brettonchen@gmail.com>
2016-05-31 21:40:07 +0800

    Change competition durations

--- main.c
+++ main.c
@@ -11,8 +11,8 @@
 #pragma platform(VEX)
 
 #pragma competitionControl(Competition)
-#pragma autonomousDuration(20)
-#pragma userControlDuration(120)
+#pragma autonomousDuration(15)
+#pragma userControlDuration(105)
 
 #include "Vex_Competition_Includes.c"
 
