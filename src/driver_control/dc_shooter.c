0ae3e3e4cbaa1625faf0cace4ccd6562f4bb7c44
qsctr <brettonchen@gmail.com>
2016-07-13 16:24:19 +0800

    Improve shooter

--- src/driver_control/dc_shooter.c
+++ src/driver_control/dc_shooter.c
@@ -1,4 +1,15 @@
 void dc_shooter() {
 
-	  motor[port6] = motor[port7] = (vexRT[Btn7R] - vexRT[Btn7L]) * 80;
+    static int prev_l = 0, prev_r = 0;
+
+    if (!prev_l && vexRT[Btn7L])
+        motor[port6] = motor[port7] = -80;
+    if (!prev_r && vexRT[Btn7R])
+        motor[port6] = motor[port7] = 80;
+    if ((prev_l && !vexRT[Btn7L]) || (prev_r && !vexRT[Btn7R]))
+        motor[port6] = motor[port7] = 0;
+
+    prev_l = vexRT[Btn7L];
+    prev_r = vexRT[Btn7R];
+
 }
