269c892039f901b97057614b664e72df31c2c597
qsctr <brettonchen@gmail.com>
2016-07-19 21:06:35 +0800

    Change dump power to 127

--- src/driver_control/dc_dump.c
+++ src/driver_control/dc_dump.c
@@ -1,11 +1,13 @@
 void dc_dump() {
 
+    // TODO: Check direction and change to U and D
+
 	static int prev_l = 0, prev_r = 0;
 
     if (!prev_l && vexRT[Btn7L])
-        dump_set(-80);
+        dump_set(-127);
     if (!prev_r && vexRT[Btn7R])
-        dump_set(80);
+        dump_set(127);
     if ((prev_l && !vexRT[Btn7L]) || (prev_r && !vexRT[Btn7R]))
        	dump_set(0);
 
