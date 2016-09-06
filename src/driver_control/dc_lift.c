fa3568170746f953c18704b733fe1aa2ae96b554
qsctr <brettonchen@gmail.com>
2016-09-06 16:55:35 +0800

    Reverse lift directions

--- src/driver_control/dc_lift.c
+++ src/driver_control/dc_lift.c
@@ -1,9 +1,9 @@
 void dc_lift() {
 
     if (vexRT[Btn7U])
-        lift_set(127);
-    else if (vexRT[Btn7D])
         lift_set(-127);
+    else if (vexRT[Btn7D])
+        lift_set(127);
     else
         lift_set(0);
 
