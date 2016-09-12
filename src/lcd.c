f811db9dd6a03c198717b68afd00eb56200e19bc
qsctr <brettonchen@gmail.com>
2016-09-12 16:31:22 +0800

    Change power expander to float

--- src/lcd.c
+++ src/lcd.c
@@ -4,7 +4,7 @@
 void lcd_voltage() {
 
     string top, bottom;
-    sprintf(top, "COR:%1.2f PE:%1.2f", nImmediateBatteryLevel * 0.001, SensorValue(PowerExpanderBattery) / 280);
+    sprintf(top, "COR:%1.2f PE:%1.2f", nImmediateBatteryLevel * 0.001, SensorValue(PowerExpanderBattery) / 280.0);
     sprintf(bottom, "BAC:%1.2f", BackupBatteryLevel * 0.001);
     displayLCDString(LCD_LINE_TOP, 0, top);
     displayLCDString(LCD_LINE_BOTTOM, 0, bottom);
