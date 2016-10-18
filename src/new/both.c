d65bd0494104b2977d9550750f933f619447af4e
qsctr <brettonchen@gmail.com>
2016-10-18 13:33:24 +0800

    typedef int motor_power

--- src/new/both.c
+++ src/new/both.c
@@ -10,34 +10,36 @@
 
 ====================================================================================================== */
 
-void base_main_set(int x);
-void base_strafe_set(int x);
-void base_rotate(int x);
-void arm_move(int x);
-void claw_move(int x);
+typedef int motor_power;
 
-void base_main_set(int x)
+void base_main_set(motor_power x);
+void base_strafe_set(motor_power x);
+void base_rotate(motor_power x);
+void arm_move(motor_power x);
+void claw_move(motor_power x);
+
+void base_main_set(motor_power x)
 {
     motor[mBaseFL] = motor[mBaseFR] = motor[mBaseBL] = motor[mBaseBR] = x;
 }
 
-void base_strafe_set(int x)
+void base_strafe_set(motor_power x)
 {
     motor[mBaseStrafe] = x;
 }
 
-void base_rotate(int x)
+void base_rotate(motor_power x)
 {
     motor[mBaseFL] = motor[mBaseBL] = x;
     motor[mBaseFR] = motor[mBaseBR] = -x;
 }
 
-void arm_move(int x)
+void arm_move(motor_power x)
 {
     motor[mArm1] = motor[mArm2] = motor[mArm3] = motor[mArm4] = x;
 }
 
-void claw_move(int x)
+void claw_move(motor_power x)
 {
     motor[mClaw] = x;
 }
